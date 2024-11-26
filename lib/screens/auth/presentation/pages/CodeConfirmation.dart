62711d07033894df07feb260b4e4d75656bdd025
Kellguzdi <20213tn114@utez.edu.mx>
2024-11-26 17:48:53 -0600

    [ADD] codeConfirmation - view: add new desgin to codeConfirmation code with a pin code

--- lib/screens/auth/presentation/pages/CodeConfirmation.dart
+++ lib/screens/auth/presentation/pages/CodeConfirmation.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:sigede_flutter/shared/widgets.dart/loading_widget.dart';
 
 class CodeConfirmation extends StatefulWidget {
   const CodeConfirmation({super.key});
@@ -8,132 +11,156 @@ class CodeConfirmation extends StatefulWidget {
   State<CodeConfirmation> createState() => _CodeConfirmationState();
 }
 
-String? validateCode(String? value) {
-  if (value == null || value.isEmpty) {
-    return 'Por favor, ingrese su código';
-  }
-
-  final int? code = int.tryParse(value);
-
-  if (code == null || code <= 0) {
-    return 'Por favor, ingrese un código válido';
-  }
-
-  return null;
-}
-
 class _CodeConfirmationState extends State<CodeConfirmation> {
   final TextEditingController _codeController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _isloading = false;
+
+  String? _code;
+  String? validateCode(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Por favor, ingrese su código';
+    }
+
+    final int? code = int.tryParse(value);
+
+    if (code == null || code <= 0) {
+      return 'Por favor, ingrese un código válido';
+    }
+
+    return null;
+  }
+
+  Future<void> _codeConfirmation() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isloading = true;
+      });
+
+      await Future.delayed(const Duration(seconds: 2));
+
+      setState(() {
+        _isloading = false;
+      });
+    }
+  }
+
+  Future<void> _checkClipboardForCode() async {
+    ClipboardData? clipboardData = await Clipboard.getData('text/plain');
+    if (clipboardData != null && clipboardData.text != null) {
+      String pastedCode = clipboardData.text!;
+      if (pastedCode.length == 6 && _isNumeric(pastedCode)) {
+        setState(() {
+          _codeController.text = pastedCode;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Código pegado automáticamente")),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("No hay un código válido en el portapapeles")),
+        );
+      }
+    }
+  }
+
+  
+  bool _isNumeric(String str) {
+    return RegExp(r'^[0-9]+$').hasMatch(str);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Recuperacion de contraseña',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: const Color(0xFF47649D),
-        centerTitle: true,
-      ),
-      body: Container(
-        color: const Color(0xFFE4E4E4),
-        child: Center(
+        appBar: AppBar(),
+        body: Center(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Container(
-              padding: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
-              ),
-              constraints: const BoxConstraints(
-                maxWidth: 600,
-              ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 25),
-                    const Text(
-                      "Codigo de verificación",
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const Text(
-                      "Por favor, ingresa el código de verificación que hemos enviado a tu correo electrónico.",
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 25),
-                    TextFormField(
-                      controller: _codeController,
-                      decoration: InputDecoration(
-                        hintText: "Ingresa tu Codigo de verificaion",
-                        label: const Text("Codigo de verificaion"),
-                        filled: true, // Activa el fondo
-                        fillColor: Colors.white, // color de fondo
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: const BorderSide(color: Colors.grey),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          // Estilos cuando no está seleccionado
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: const BorderSide(color: Colors.grey),
-                        ),
-                      ),
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [
-                        FilteringTextInputFormatter.digitsOnly,
-                      ],
-                      maxLength: 6,
-                      validator: validateCode,
-                    ),
-                    const SizedBox(
-                      height: 32,
-                    ),
-                    SizedBox(
-                      height: 48,
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            print("AQUI VA LA LOGICA DEL VALIDACION DE CODIGO");
-                            Navigator.pushNamed(context, '/resetPassword');
-                          }
-                        },
-                        style: OutlinedButton.styleFrom(
-                          backgroundColor: const Color(0xFF27AE60),
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        child: const Text(
-                          "Verificar código",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 25),
-                  ],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Verificar código',
+                  style: GoogleFonts.rubikMonoOne(
+                      textStyle: const TextStyle(
+                    fontSize: 30,
+                  )),
                 ),
-              ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  'Para restablecer tu contraseña, por favor ingresa el código de verificación que hemos enviado a tu correo electrónico',
+                  style: GoogleFonts.roboto(
+                      textStyle: const TextStyle(color: Colors.grey)),
+                ),
+                const SizedBox(
+                  height: 36.0,
+                ),
+                Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        PinCodeTextField(
+                            appContext: context,
+                            length: 6, 
+                            controller: _codeController,
+                            onChanged: (value) {
+                              setState(() {
+                                _code = value;
+                              });
+                            },
+                            onTap: () async {
+                              await _checkClipboardForCode();
+                            },
+                            validator: validateCode,
+                            textStyle: const TextStyle(color: Colors.black),
+                            pinTheme: PinTheme(
+                              shape: PinCodeFieldShape.box,
+                              borderRadius: BorderRadius.circular(8),
+                              fieldHeight: 50,
+                              fieldWidth: 40,
+                              activeColor: Colors.black,
+                              selectedColor: Colors.red,
+                              inactiveColor: Colors.red,
+                            ),
+                            keyboardType: TextInputType.number,
+                            enablePinAutofill: true),
+                      ],
+                    ),
+                  ),
+                ),
+                const Expanded(child: Column()),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 48,
+                    child: ElevatedButton(
+                      onPressed: _isloading ? null : _codeConfirmation,
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                      ),
+                      child: _isloading
+                          ? const LoadingWidget() // Mostrar loading si está cargando
+                          : Text(
+                              'Enviar',
+                              style: GoogleFonts.roboto(
+                                textStyle: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
