b8280537af96ae698fc35b2029c2c0c548efc60d
Kellguzdi <20213tn114@utez.edu.mx>
2024-12-06 10:41:24 -0600

    [DELETE] All entities

--- lib/modules/superadmin/domain/entities/admin_update_entity.dart
+++
@@ -1,7 +0,0 @@
-class AdminUpdateEntity {
-  final int userId;
-  final String name;
-  final int fkStatus;
-
-  AdminUpdateEntity({required this.userId, required this.name, required this.fkStatus});
-}
